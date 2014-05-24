6dc5c58dd8a2043ca94e4ca0821f6eb863aa6961
firefly431 <firefly431.awesome@gmail.com>
2014-05-24 15:24:59 -0500

    Server

--- BH Server/src/Main.java
+++ BH Server/src/Main.java
@@ -1,14 +1,20 @@
 import com.sun.net.httpserver.*;
+
+import java.io.IOException;
 import java.net.*;
 
-public class Main() {
-    public static void main(String argv[]) {
+public class Main {
+    public static void main(String argv[]) throws Exception {
         HttpServer s = HttpServer.create(new InetSocketAddress(8080), 5);
         s.createContext("/", new HttpHandler() {
             public void handle(HttpExchange x) {
-                if (x.getRequestMethod() != "GET") {
-                    x.getRequestBody().close();
-                    x.sendResponseHeaders(400, -1);
+                try {
+                    if (!x.getRequestMethod().equals("GET")) {
+                        x.getRequestBody().close();
+                        x.sendResponseHeaders(400, -1);
+                    }
+                } catch (IOException e) {
+                    // oh no
                 }
             }
         });
