31512f8d79a1b306980998764eb7b27d13f16482
willsherwood <will.sherwood5@gmail.com>
2014-05-24 16:05:22 -0500

    client sherwood

--- BH Client/src/Client.java
+++ BH Client/src/Client.java
@@ -1,4 +1,5 @@
 import java.awt.Graphics;
+import java.awt.image.BufferedImage;
 
 import javax.swing.JApplet;
 
@@ -7,9 +8,17 @@ import chessboard.ChessBoard;
 public class Client extends JApplet {
 
 	private ChessBoard a;
+	private ChessBoard b;
+	
+	private BufferedImage aa;
+	private BufferedImage bb;
 	
 	public void init() {
 		a = new ChessBoard();
+		b = new ChessBoard();
+		aa = new BufferedImage(60*8, 60*8, BufferedImage.TYPE_INT_RGB);
+		bb = new BufferedImage(60*8, 60*8, BufferedImage.TYPE_INT_RGB);
+		
 	}
 	
 	public void start() {
@@ -25,6 +34,9 @@ public class Client extends JApplet {
 	}
 	
 	public void paint(Graphics g) {
-		a.drawBoard(g, 60, true);
+		a.drawBoard(aa.getGraphics(), 60, true);
+		b.drawBoard(bb.getGraphics(), 60, false);
+		g.drawImage(aa, 0, 0, null);
+		g.drawImage(bb, 60*8+24, 0, null);
 	}
 }
